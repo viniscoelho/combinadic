da296c569a7a09b7aa74359da3718f8f56a611fe
Vinicius Coelho <vinicius.coelho@sap.com>
2019-10-10 14:37:42 -0300

    Fix formatting of files

--- combinadic.h
+++ combinadic.h
@@ -3,8 +3,7 @@
 typedef long long int64;
 #endif
 
-__device__ class Combination
-{
+__device__ class Combination {
 private:
     int n, k;
     int data[4];
@@ -14,15 +13,15 @@ public:
     __device__ Combination(int n, int k)
     {
         // n >= k
-        if (n < 0 || k < 0)
-        {
+        if (n < 0 || k < 0) {
             printf("Negative parameter in constructor\n");
             return;
         }
 
         this->n = n;
         this->k = k;
-        for (int i = 0; i < k; ++i) data[i] = i;
+        for (int i = 0; i < k; ++i)
+            data[i] = i;
     }
 
     __device__ Combination(int n, int k, int* a)
@@ -30,44 +29,44 @@ public:
         this->n = n;
         this->k = k;
 
-        for (int i = 0; i < k; ++i) data[i] = a[i];
+        for (int i = 0; i < k; ++i)
+            data[i] = a[i];
 
-        if (!isValid())
-        {
+        if (!isValid()) {
             printf("Bad value from array!\n");
             return;
         }
     }
-    
-    __device__ Combination(){ }
+
+    __device__ Combination() {}
 
     __device__ static int64 choose(int n, int k)
     {
-        if (n < 0 || k < 0)
-        {
+        if (n < 0 || k < 0) {
             printf("Invalid negative parameter in choose()\n");
             return -1;
         }
-        if (n < k) return 0;  // special case
-        if (n == k) return 1;
+        if (n < k)
+            return 0; // special case
+        if (n == k)
+            return 1;
 
         int64 delta, iMax;
 
-        if (k < n-k) // ex: choose(100, 3)
+        if (k < n - k) // ex: choose(100, 3)
         {
-            delta = n-k;
+            delta = n - k;
             iMax = k;
-        }
-        else         // ex: choose(100, 97)
+        } else // ex: choose(100, 97)
         {
             delta = k;
-            iMax = n-k;
+            iMax = n - k;
         }
 
         int64 ans = delta + 1;
 
         for (int64 i = 2; i <= iMax; ++i)
-            ans = (ans * (delta + i)) / i; 
+            ans = (ans * (delta + i)) / i;
 
         return ans;
     }
@@ -76,19 +75,17 @@ public:
     __device__ Combination successor();
     __device__ Combination element(int64);
     __device__ int* getArray();
-
 };
 /*
     Returns if it is a valid combination
     */
 __device__ bool Combination::isValid()
 {
-    for (int i = 0; i < k; ++i)
-    {
+    for (int i = 0; i < k; ++i) {
         if (data[i] < 0 || data[i] > n - 1)
             return false; // value out of range
 
-        for (int j = i+1; j < k; ++j)
+        for (int j = i + 1; j < k; ++j)
             if (data[i] >= data[j])
                 return false; // duplicate or not lexicographic
     }
@@ -101,7 +98,8 @@ __device__ bool Combination::isValid()
 __device__ int64 Combination::largestV(int a, int b, int64 x)
 {
     int v = a - 1;
-    while (choose(v, b) > x) --v;
+    while (choose(v, b) > x)
+        --v;
 
     return v;
 }
@@ -110,7 +108,8 @@ __device__ int64 Combination::largestV(int a, int b, int64 x)
     */
 __device__ Combination Combination::successor()
 {
-    if (data[0] == n - k) return Combination(0, 0);
+    if (data[0] == n - k)
+        return Combination(0, 0);
 
     Combination ans(n, k);
 
@@ -118,12 +117,13 @@ __device__ Combination Combination::successor()
     for (i = 0; i < k; i++)
         ans.data[i] = data[i];
 
-    for (i = k - 1; i > 0 && ans.data[i] == (n - k + i); --i);
+    for (i = k - 1; i > 0 && ans.data[i] == (n - k + i); --i)
+        ;
 
     ++ans.data[i];
 
     for (int j = i; j < k - 1; j++)
-        ans.data[j+1] = ans.data[j] + 1;
+        ans.data[j + 1] = ans.data[j] + 1;
 
     return ans;
 }
@@ -138,16 +138,15 @@ __device__ Combination Combination::element(int64 m)
     int b = k;
     int64 x = (choose(n, k) - 1) - m; // x is the "dual" of m
 
-    for (int i = 0; i < k; ++i)
-    {
-        ans[i] = largestV(a, b, x); // largest value v, where v < a and vCb < x    
+    for (int i = 0; i < k; ++i) {
+        ans[i] = largestV(a, b, x); // largest value v, where v < a and vCb < x
         x = x - choose(ans[i], b);
         a = ans[i];
-        b = b-1;
+        b = b - 1;
     }
 
     for (int i = 0; i < k; ++i)
-        ans[i] = (n-1) - ans[i];
+        ans[i] = (n - 1) - ans[i];
 
     return Combination(n, k, ans);
 }
